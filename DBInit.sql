b992c8741a24c48583cf33134fdc742c03981205
Saad-Driouech <73725166+Saad-Driouech@users.noreply.github.com>
2021-12-12 00:32:10 +0100

    Database populated

--- DBInit.sql
+++ DBInit.sql
@@ -1,3 +1,5 @@
+-- Dropping tables if they exist
+
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Admin;
 DROP TABLE IF EXISTS Student;
@@ -9,16 +11,24 @@ DROP TABLE IF EXISTS Offering;
 DROP TABLE IF EXISTS Session;
 DROP TABLE IF EXISTS Location;
 
+------------------------------------------------------------------------------------------
 
+-- Creating tables
+
+CREATE TABLE Account(
+	user_id smallint,
+	password varchar(20)
+);
 CREATE TABLE User(
-	user_id varchar(8) PRIMARY KEY,
+	user_id smallint PRIMARY KEY,
 	f_name varchar(15) not NULL,
 	l_name varchar(15) not NULL,
-	password varchar(8) not NULL,
 	phone_num char(10),
 	DOB date not NULL,
 	user_type char(1) not NULL,
 	CONSTRAINT CkUser_type CHECK (user_type in ('A', 'S', 'T'))
+	CONSTRAINT accountfk FOREIGN KEY (user_id) REFERENCES Account(user_id) 
+		ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 CREATE TABLE Admin(
@@ -31,10 +41,10 @@ CREATE TABLE Admin(
 
 CREATE TABLE Student(
 	PRIMARY KEY(user_id),
-	attended_hours decimal(5,2) not NULL DEFAULT 0,
-	stud_balance decimal(4, 2) not NULL,
-	reg_date date not NULL,
-	academic_level varchar(5) not NULL,
+	attended_hours decimal(5,2) NOT NULL DEFAULT 0,
+	stud_balance decimal(4, 2) NOT NULL,
+	reg_date date NOT NULL,
+	academic_level varchar(5) NOT NULL,
 	CONSTRAINT Ckacademic_level CHECK (academic_level in ('TC', '1BAC', '2BAC'))
 )INHERITS user;
 
@@ -49,6 +59,8 @@ CREATE TABLE Availability(
 	av_date date not NULL,
 	start_time time NOT NULL, 
 	end_time time NOT NULL,
+	tutor_id smallint NOT NULL,
+	CONSTRAINT timeConstraint CHECK (start_time < end_time),
 	CONSTRAINT available_at FOREIGN KEY (tutor_id) REFERENCES Tutor(user_id)
                       ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -64,8 +76,7 @@ CREATE TABLE Booking(
    	CONSTRAINT Ckacademic_level CHECK (rating BETWEEN 0 AND 5)
 );
 
-CREATE TABLE Course(
-
+CREATE TABLE Course(course_code, title, level, price_hr) VALUES
 	course_code VARCHAR(8) PRIMARY KEY,
 	title VARCHAR(10) not null,
 	level VARCHAR(10) not null,
@@ -94,6 +105,7 @@ CREATE TABLE Session(
 	tutor_ID VARCHAR(8),
 	room_num int 
 	CONSTRAINT CKstatus CHECK (status IN ('Open', 'Closed', 'Canceled')),
+   	CONSTRAINT timeConstraint CHECK (start_time < end_time),
 	CONSTRAINT corresponds FOREIGN KEY (course_code) REFERENCES OFFERING(course_code)
         		ON UPDATE CASCADE ON DELETE CASCADE
     	CONSTRAINT tutored_by FOREIGN KEY (tutor_ID) REFERENCES OFFERING(tutor_ID)
@@ -104,7 +116,108 @@ CREATE TABLE Session(
 );
 
 CREATE TABLE Location(
-	room_num int PRIMARY KEY,
+	room_num smallint PRIMARY KEY,
 	room_seats smallint not NULL DEFAULT 14, 
 );
 
+------------------------------------------------------------------------------------------
+
+-- Inserting into tables
+
+INSRERT INTO Account(user_id, password) VALUES
+  (100, 'saaddr'),
+  (101, 'yassirbe'),
+  (102, 'khaoulaai'),
+  (103, 'nadabo'),
+  (104, 'younesja'),
+  (1, 'rafahbe'),
+  (10, 'lailamo'),
+  (11, 'mouadel'),
+  (20, 'anassde'),
+  (50, 'hassanro'),
+  (51, 'wahibakh'),
+  (52, 'naimala'),
+  (53, 'omarbo'),
+  (54, 'fouadzi'),
+  (55, 'abdelmajidrb')
+
+INSERT INTO Student(user_id, f_name, l_name, password, phone_num, DOB, user_type) VALUES
+  (100, 'Saad', 'Driouech', '0662097271', '2000-09-15', 'S'),
+  (101, 'Yassir, 'Bendabdellah', '0654789622', '2000-06-14', 'S'),
+  (102, 'Khaoula', 'Ait Soussi', '0614859473', '2000-12-08', 'S'),
+  (103, 'Nada', 'Bounajma', '0638749122', '2001-01-22', 'S'),
+  (104, 'Younes', 'Jamal', '0754399985', '2000-10-01', 'S'),
+  (1, 'Rafah', 'Bennani', '0512846931', '1982-03-28', 'A'), 
+  (10, 'Laila', 'Moustaghit', '0546952132', '1990-06-12', 'A'),
+  (20, 'Mouad', 'Elazizi', '0548623597', '1988-04-30', 'A'),
+  (21, 'Anass', 'Dehbi', '0678549622', '1991-10-24', 'A'),
+  (50, 'Hassan', 'Rouias', '0654896755', '1975-02-14', 'T'),
+  (51, 'Wahiba', 'Khassim', '0696342100', '1983-03-03', 'T'),
+  (52, 'Naima', 'Laalami', '0675896314', '1978-08-15', 'T'),
+  (53, 'Omar', 'Bouchta', '0666128684', '1976-06-06', 'T'),
+  (54, 'Fouad', 'Ziani, '0658669744', '1979-07-12', 'T'),
+  (55, 'Abdelmajid', 'Rbib', '0685479123','1976-08-12', 'T');
+
+INSERT INTO Admin (user_id, admin_role, 'hiring_date') VALUES
+  (1, 'O', '2018-09-01'),
+  (10, 'M', '2018-09-15'),
+  (20, 'O', '2018-09-15'),
+  (21, 'O', '2019-04-01');
+
+INSERT INTO Tutor(user_id) VALUES
+  (50),
+  (51),
+  (52),
+  (53),
+  (54),
+  (55);
+
+INSERT INTO Availability(av_code, av_date, start_time, end_time, tutor_id) VALUES
+  ('1', '2021-12-20', '18:00:00', '20:00:00', 50),
+  ('2', '2021-12-21', '19:00:00', '20:30:00', 50),
+  ('3', '2021-12-21', '19:00:00', '20:30:00', 51),
+  ('4', '2021-12-21', '18:00:00', '20:00:00', 51),
+  ('5', '2021-12-20', '19:00:00', '20:30:00', 52),
+  ('6', '2021-12-22', '18:30:00', '20:30:00', 53),
+  ('7', '2021-12-22', '20:30:00', '21:30:00', 53),
+  ('8', '2021-12-22', '18:30:00', '20:30:00', 54),
+  ('9', '2021-12-20', '18:30:00', '19:30:00', 50);
+
+INSERT INTO Course (course_code, title, level, price_hr) VALUES 
+  ('MTH1', 'Math 1 BAC', '1BAC', 250.00),
+  ('MTH2', 'Math 2 BAC', '2BAC', 300.00),
+  ('PHY1', 'Physique 1 BAC', '1BAC', 200.00),
+  ('PHY1', 'Physique 2 BAC', '2BAC', 280.00),
+  ('MTH0', 'Math tronc commun', 'TC', 150.00),
+  ('PHY0', 'Physique TC', 'TC', 120.00),
+  ('SVT0', 'Science de Vie et Terre TC', 'TC', 100.00),
+  ('SVT1', 'Science de Vie et Terre 1 BAC', '1BAC', 180.00),
+  ('SVT2', 'Science de Vie et Terre 2 BAC', '2BAC', 220.00);
+
+INSERT INTO Offering(tutor_id, course_code) VALUES
+  (50, 'MTH1'),
+  (50, 'MTH0'),
+  (51, 'MTH2'),
+  (52, 'PHY0'),
+  (52, 'PHY1'),
+  (52, 'PHY2'),
+  (53, 'SVT0'),
+  (53, 'SVT1'),
+  (54, 'SVT1'),
+  (54, 'SVT2');
+
+INSERT INTO Location(room_num, room_seats) VALUES
+  (1, 15),
+  (2, 7),
+  (3, 18),
+  (4, 14);
+
+INSERT INTO Session(session_code, session_date, start_time, end_time, seats_av, status, course_code, tutor_id, room_num) VALUES
+  ('ses1', '2021-12-20', '19:00:00', '20:00:00', 10, 'O', 'MTH1', 50, 1),
+  ('ses2', '2021-12-20', '19:00:00', '20:00:00', 10, 'O', 'MTH1', 50, 1);
+
+INSERT INTO Booking(student_id, session_code, rating) VALUES
+  (100, 'ses1', 5),
+  (101, 'ses1', 4),
+  (103, 'ses1', 5),
+  (102, 'ses2', 3);
